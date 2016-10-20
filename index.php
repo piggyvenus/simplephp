f43d83e4397b1da7ee54112a924b17c3b82967e6
piggyvenus <piggyvenus@gmail.com>
2016-10-20 15:25:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-echo "Welcome to the best application in the world.<br>";
-echo "Welcome to the best application in the world2.<br>";
-echo "Welcome to the best application in the world3.<br>";
-echo "Welcome to the best application in the world.<br>";
-echo "Welcome to the best application in the world2.<br>";
-echo "Welcome to the best application in the world3.<br>";
+echo "<H1>Welcome to the PHPAPP TWO application!</H1><br>";
 
 
 
