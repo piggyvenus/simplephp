14c1ba3bf19ad76b19924c53dedd2f6f4e093893
piggyvenus <piggyvenus@gmail.com>
2017-06-13 11:38:24 -0400

    live update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<H1>Welcome to Red Hat OpenShift @ Solarflare Contain NY! version 0.2</H1><br>";
+echo "<H1>Welcome to Red Hat OpenShift @ Solarflare Contain NY! version 0.3</H1><br>";
 
 
 
